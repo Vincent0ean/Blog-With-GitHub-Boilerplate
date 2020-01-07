1f5bf3307ab58132e8a0d41d1a19bcf2ae3358fe
Vincent0ean <35303441+Vincent0ean@users.noreply.github.com>
2020-01-07 10:29:54 +0800

    Update conf.py

--- conf.py
+++ conf.py
@@ -14,14 +14,14 @@ enable_jsdelivr = {
 }
 
 # 站点设置
-site_name = "我的个人博客"
+site_name = "足迹"
 site_logo = "${static_prefix}logo.png"
 site_build_date = "2019-12-18T16:51+08:00"
-author = "熊猫小A"
-email = "hi@imalan.cn"
+author = "vind"
+email = "vincent0ean@gmail.com"
 author_homepage = "https://www.imalan.cn"
-description = "只坚持一种正义。我的正义。"
-key_words = ['Maverick', '熊猫小A', 'Galileo', 'blog']
+description = "每天都在给自己输入信息，这会儿我就来输出一下。"
+key_words = ['vind', 'blog']
 language = 'zh-CN'
 external_links = [
     {
