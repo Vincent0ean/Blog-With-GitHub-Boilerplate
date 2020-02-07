3fb486771c65703007e3dbe46c8cd5b4d8f62364
Vincent0ean <35303441+Vincent0ean@users.noreply.github.com>
2020-02-07 17:09:07 +0800

    Update conf.py

--- conf.py
+++ conf.py
@@ -15,7 +15,7 @@ enable_jsdelivr = {
 
 # 站点设置
 site_name = "足迹"
-site_logo = "${static_prefix}logo.png"
+site_logo = "${static_prefix}static/logo.png"
 site_build_date = "2019-12-18T16:51+08:00"
 author = "vind"
 email = "vincent0ean@gmail.com"
